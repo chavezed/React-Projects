147ebc4e0be4eefff509bff53ffa9f3814ecc7bd
chavezed <echavez9@ucsc.edu>
2020-01-31 23:00:53 -0800

    initial commit

---
+++ codecamp-tutorial/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './style.css';
+import MyInfo from "./components/MyInfo"; // needs a relative path (.js optional)
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
