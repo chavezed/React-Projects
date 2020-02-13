8ea310adac3ec0536e2dfe5f89d27e8180a8c2e7
chavezed <echavez9@ucsc.edu>
2020-02-12 21:26:27 -0800

    todo completed

--- codecamp-tutorial/src/Footer.js
+++ codecamp-tutorial/src/Footer.js
@@ -1,9 +1,11 @@
 import React from "react"
 
-function Footer() {
-    return (
-        <header>I am the footer!</header> 
-    )
+class Footer extends React.Component {
+    render() {
+        return (
+            <header>I am the footer!</header> 
+        )
+    }
 }
 
 export default Footer
